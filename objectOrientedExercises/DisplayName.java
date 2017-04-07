ba2e86e5708b23f2eeaadf604225a47e32a66515
Ramesh <ramesh4ever.ramesh1@gmail.com>
2017-04-07 13:58:10 +0200

    Create method in one class, and call in other class

---
+++ objectOrientedExercises/DisplayName.java
@@ -0,0 +1,11 @@
+package objectOrientedExercises;
+
+public class DisplayName {
+	/*
+	 * Call method of Name class
+	 */
+	public static void main(String[] args) {
+		Name reference = new Name();
+		reference.displayName();
+	}
+}
---
+++ objectOrientedExercises/Name.java
@@ -0,0 +1,13 @@
+package objectOrientedExercises;
+
+public class Name {
+
+	/*
+	 * Write Text‐Based Application using Object‐Oriented Approach to display your name.
+	 */
+	
+	public void displayName(){
+		System.out.println("Ramesh Kumar");
+	}
+	
+}
